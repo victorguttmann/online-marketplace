f38fec61ce83040df00778daadd9641b0fe79c34
Student <example@example.com>
2019-08-19 16:56:40 -0700

    js prompt of user address and name not prompting

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document)ready(function() {
+
+var add1 = $("#addressLine1").val();
+var add2 = $("#addressLine2").val();
+var add3 = $("#addressLine3").val();
+
+var nam1 = $("#userName").val();
+
+nam1.prompt();
+});
